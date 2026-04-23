718316c6172a7d0a75f756dddd8c9b828536eaa1
Sarthakdoke17 <black20075001@gmail.com>
2026-04-23 14:42:01 +0530

    Update program7.py

--- program7.py
+++ program7.py
@@ -1,15 +1,15 @@
 # Base class
 class Vehicle:
  def move(self):
- print("Vehicle is moving")
+  print("Vehicle is moving")
 # Subclass Car
 class Car(Vehicle):
  def move(self):
- print("Driving on the road")
+  print("Driving on the road")
 # Subclass Bicycle
 class Bicycle(Vehicle):
  def move(self):
- print("Pedaling on the road")
+  print("Pedaling on the road")
 # Demonstrating polymorphism
 vehicles = [Car(), Bicycle()]
 for v in vehicles:
